e1ea38a0512cccfc3bc15990b222abb1efe7f2d1
Qansa Alghifari <46017824+Qansa2916@users.noreply.github.com>
2019-11-04 10:52:50 +0700

    Tugas_24_JS

---
+++ Tugas-24.js
@@ -0,0 +1,10 @@
+function tugas24() {
+    var number = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+
+    console.log("Sebelumnya: ", number);
+    var urut = number.sort();
+    console.log("Ascending: ", urut);
+    console.log("Descending: ", number.reverse());
+
+}
+tugas24();
